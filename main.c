c384ccf994a9f0b0bcfb2c7cdb9d8ef3b6da9220
João Pedro Silva <jsangregorio@hotmail.com>
2016-06-09 22:16:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,66 +3,105 @@
 
 
 // STRUCTS
-struct personagem {
-char nome[20];
-int level;
-int hp;
-int vitalidade;
-int forca;
-int resistencia;
-int gold;
-
-
-};
-struct enemy {
-char nome[20];
-int level;
-int hp;
-int vitalidade;
-int forca;
-int resistencia;
-};
-
-
-
-
-
-int main () {
-
-
-
-int request;
-int location; // 0 - cidade ; 1 - lutando; 2 - resting;
-int monlevel;
-
-switch(request) {
-
-case 0: // start a new game
-
+struct personagem
 {
-struct personagem player;
+    char nome[20];
+    int level;
+    int hp;
+    int vitalidade;
+    int forca;
+    int resistencia;
+    int gold;
+
+
+};
+struct enemy
+{
+    char nome[20];
+    int level;
+    int hp;
+    int vitalidade;
+    int forca;
+    int resistencia;
+};
+
+void incrementlevel();
+
+
+
+
+int main ()
+{
+
+
+
+    int request;
+    int location; // 0 - cidade ; 1 - lutando; 2 - resting;
+    int monlevel;
+
+    switch(request)
+    {
+
+    case 0: // start a new game
+
+    {
+        struct personagem player;
+    }
+
+    break;
+    case 1: // change location
+
+        break;
+
+    case 2: // on monster death
+
+        break;
+
+    case 3: // recover hp
+        player.hp = player.vitalidade;
+        break;
+
+    case 4: //increment status
+
+        break;
+
+
+    }
+
+
+    return 0;
 }
 
-break;
-case 1: // change location
-
-break;
-
-case 2: // on monster death
-
-break;
-
-case 3: // recover hp
-player.hp = player.vitalidade;
-break;
-
-case 4: //increment status
-
-break;
 
 
+// funções
+
+void incrementlevel (struct *personagem player))
+{
+
+    player->hp = (player->hp) + 1;
 }
 
+void incrementlevel (struct *player, int status, int qnt)
+{
+// 0 = força; 1 = vitalidade; 2 = resistencia;
+    switch(status)
+    case 0:
+{
+    player->forca += 1;
+    }
 
-return 0;
+    break;
+case 1:
+    {
+        player->vitalidade  += 1;
+    }
+
+    break;
+case 2:
+    {
+        player->resistencia  += 1;
+    }
+
+    break;
 }
