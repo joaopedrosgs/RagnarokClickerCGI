ee12a61f26f327640cef7706f4a5a98506f378b4
João Pedro Silva <jsangregorio@hotmail.com>
2016-06-12 11:01:34 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,59 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct html {
+struct html
+{
 
- void (*createElement) (char* tag, char* classe, char* content);
+    void (*createElement) (char* tag, char* classe, char* content);
 
- void (*header) (char* title, char* cssfile);
+    void (*header) (char* title, char* cssfile);
 
-  void (*ending)();
-}html;
+    void (*ending)();
+} html;
 
 void createElement(char* tag, char* classe, char* content);
 void header (char* title, char* cssfile);
 void ending();
 
-int main(){
+int main()
+{
 
-html.createElement = createElement;
-html.header = header;
-html.ending = ending;
-html.header("Titulo", "css/master.css");
-html.createElement("div", "classeloca", "ola mundo");
-html.ending();
+    html.createElement = createElement;
+    html.header = header;
+    html.ending = ending;
+    html.header("Titulo", "css/master.css");
+    html.createElement("div", "classeloca", "shampoo");
+    html.ending();
 
-return 0;}
+    return 0;
+}
 
 
 // ------------------------------------------------------------
 
 
-void createElement(char* tag, char* classe, char* content) {
-printf("<%s class='%s'>%s</%s>\n", tag, classe, content, tag);
+void createElement(char* tag, char* classe, char* content)
+{
+    printf("<%s class='%s'>%s</%s>\n", tag, classe, content, tag);
 
 }
 
-void header (char* title, char* cssfile) {
-printf("%s%c%c\n","Content-Type:text/html;charset=UTF-8",13,10);
+void header (char* title, char* cssfile)
+{
+    printf("%s%c%c\n","Content-Type:text/html;charset=UTF-8",13,10);
 
-  printf("<!DOCTYPE html>\n");
+    printf("<!DOCTYPE html>\n");
 
-  printf("<html lang=\"pt\">\n");
+    printf("<html lang=\"pt\">\n");
 
-  printf("<head>");
+    printf("<head>");
 
-  printf("<meta charset=\"utf-8\">\n");
-  printf("<title>%s</title>\n", title);
-  printf("<link rel=\"stylesheet\" href=\"%s\">\n", cssfile);
+    printf("<meta charset=\"utf-8\">\n");
+    printf("<title>%s</title>\n", title);
+    printf("<link rel=\"stylesheet\" href=\"%s\">\n", cssfile);
     //printf("<script src=\"script.js\"></script>");
 
-      printf("</head>\n");
+    printf("</head>\n");
 
-      printf("<body>\n");
+    printf("<body>\n");
 }
 
-void ending() {
-printf("</body>\n");
-printf("</html>\n");
+void ending()
+{
+    printf("</body>\n");
+    printf("</html>\n");
 }
