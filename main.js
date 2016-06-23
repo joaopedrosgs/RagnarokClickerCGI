45b093f461fca47d5814ece85c874f7820c26f7d
João Pedro Silva <jsangregorio@hotmail.com>
2016-06-23 15:12:07 -0300

    Add files via upload
    bug do hp arrumado

--- main.js
+++ main.js
@@ -1,83 +1,94 @@
-var actualhppx =  parseFloat($("#enemyhpfill").css('width'));
-var totalhppx =  parseFloat($(".hp").css('width'));
-var actualhp = actualhppx/totalhppx;
+var actualhppx = parseFloat($("#enemyhpfill").css('width'));
+var totalhppx = parseFloat($("#enemyhp").css('width'));
+var actualhp = actualhppx / totalhppx;
 var totalhp = 100;
 var damage = 0;
 
 var monstro;
-var player = {level:12, hp:100, forca:10,attack: 20, resistencia:12, gold:100};
-function getmonster(){
+var player = {
+    level: 12,
+    hp: 100,
+    forca: 10,
+    attack: 20,
+    resistencia: 12,
+    gold: 100
+};
 
-  $.ajaxSetup({
-    async: false
-});
-     $.getJSON('http://localhost/cgi-bin/main.cgi?request=2', function (data) {
+function getmonster() {
+
+    $.ajaxSetup({
+        async: false
+    });
+    $.getJSON('http://cap.dc.ufscar.br/~726549/cgi-bin/main.cgi?request=2', function(data) {
         monstro = data;
-          $(".monstro").css({ "background-image":"url('css/monsprite/"+ monstro.sprite +".gif')"});
+        $(".monstro").css({
+            "background-image": "url('css/monsprite/" + monstro.sprite + ".gif')"
+        });
 
-  
- 
-      });
+
+
+    });
 
 }
-function fillenemyhp(){
 
-      actualhppx = totalhppx;
-          actualhp = actualhppx/totalhppx;
-          $("#enemyhpfill").css('width', ''+ (actualhp*100) +'%');
+function fillenemyhp() {
+
+    actualhppx = totalhppx;
+    actualhp = actualhppx / totalhppx;
+    $("#enemyhpfill").css('width', '' + (actualhp * 100) + '%');
 
 }
 
 getmonster();
 
 
-function updateuser(){
-	$(".gold").html(player.gold);
+function updateuser() {
+    $(".gold").html(player.gold);
 
 }
 
 
 function attack(e) {
-damage = Math.floor(Math.random() * player.attack) + player.attack/2; 
-lastdamage = damage; 
-actualhp = parseFloat((((monstro.hp*actualhp) - damage)/monstro.hp).toFixed(2));
-$("#enemyhpfill").css('width', ''+ (actualhp*100) +'%');
+    damage = Math.floor(Math.random() * player.attack) + player.attack / 2;
+    lastdamage = damage;
+    actualhp = parseFloat((((monstro.hp * actualhp) - damage) / monstro.hp).toFixed(2));
+    $("#enemyhpfill").css('width', '' + (actualhp * 100) + '%');
 
-console.log(actualhp);
-if(actualhp <= 0)  {
-fillenemyhp();
-getmonster();
+    console.log(actualhp);
+    if (actualhp <= 0) {
+        fillenemyhp();
+        getmonster();
 
-updateuser();
+        updateuser();
 
-}
+    }
 
-console.log(monstro.sprite);
+    console.log(monstro.sprite);
 
-e.preventDefault;
+    e.preventDefault;
 
-  // -> removing the class
-  divhit.classList.remove("hit");
-  
-  // -> triggering reflow /* The actual magic */
-  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-  divhit.offsetWidth = divhit.offsetWidth;
-  
-  // -> and re-adding the class
-  divhit.classList.add("hit");
+    // -> removing the class
+    divhit.classList.remove("hit");
 
-      var el     = $(".effect"),
+    // -> triggering reflow /* The actual magic */
+    // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+    divhit.offsetWidth = divhit.offsetWidth;
+
+    // -> and re-adding the class
+    divhit.classList.add("hit");
+
+    var el = $(".effect"),
         newone = el.clone(true);
 
     el.before(newone);
 
     $(".attack:last").remove();
 
-    $('.monstro').prepend("<div class='attackbubble'>"+ lastdamage +"</div>");
-setTimeout(function() {
-  $('.attackbubble:last').remove();
-}, 500);
-}	
+    $('.monstro').prepend("<div class='attackbubble'>" + lastdamage + "</div>");
+    setTimeout(function() {
+        $('.attackbubble:last').remove();
+    }, 500);
+}
 
 
 //--------------------------
@@ -85,10 +96,7 @@ divhit = document.getElementById("monstro");
 
 
 divhit.addEventListener("click", function(e) {
-   attack(e);
-  
+    attack(e);
+
 
 }, false);
-
-
-
